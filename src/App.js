bfc4d91d4530256cc5ff63dbaf5f3522384130de
Anne Whitman <aniel928@gmail.com>
2023-07-09 15:05:43 -0400

    add router library and simple routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Route, Routes } from 'react-router-dom';
+
+// App Components
+import About from './components/About';
+import Courses from './components/Courses';
+import Header from './components/Header';
+import Home from './components/Home';
+import Teachers from './components/Teachers';
 
 function App() {
     return (
         <div className="container">
-
+            <Header />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/teachers' element={<Teachers />} />
+                <Route path='/courses' element={<Courses />} />
+            </Routes>
         </div>
     );
 }
