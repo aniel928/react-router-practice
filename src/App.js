972df01fcd8ee91119544fd5e3c27c2fe26e421e
Anne Whitman <aniel928@gmail.com>
2023-07-09 16:08:52 -0400

    create a featured page and redirect on form submit using routes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 // App Components
 import About from './components/About';
 import Courses from './components/Courses';
+import Featured from './components/Featured';
 import Header from './components/Header';
 import Home from './components/Home';
+import NotFound from './components/NotFound';
 import Teachers from './components/Teachers';
 
 import CourseContainer from './components/courses/CourseContainer';
@@ -13,18 +15,18 @@ import { HTMLCourses } from './data/courses';
 import { JSCourses } from './data/courses';
 import { CSSCourses } from './data/courses';
 
-
-import NotFound from './components/NotFound';
-
 function App() {
     return (
         <div className="container">
             <Header />
             <Routes>
                 <Route path='/' element={<Home />} />
-                <Route path='/about/' element={<About />} />
-                <Route path='/teachers/' element={<Teachers />} />
-                <Route path='/courses/' element={<Courses />}>
+                <Route path='about' element={<About />} />
+                <Route path='teachers' element={<Teachers />} />
+                {/* <Route index element={<Teachers />} /> */}
+                <Route path='teachers/:name/:course' element={<Featured />} />
+
+                <Route path='courses' element={<Courses />}>
                     <Route index element={<Navigate replace to="html" />} />
                     <Route path='html' element={<CourseContainer data={HTMLCourses} />} />
                     <Route path='css' element={<CourseContainer data={CSSCourses} />} />
--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Featured = () => {
+    let { course, name } = useParams();
+    name = name.replace('-', ' ');
 
-  return (
-    <div className="main-content">
-      <h2>Featured: </h2>
-      <p>Introducing <strong></strong>, a teacher who loves teaching courses about <strong></strong>!</p>
-    </div>
-  );
+    return (
+        <div className="main-content">
+            <h2>{name}
+            </h2>
+            <p>Introducing <strong>{name}</strong>, a teacher who loves teaching courses about <strong>{course}</strong>!</p>
+        </div>
+    );
 }
 
 export default Featured;
