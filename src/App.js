373d5eb570b623eb4d9521f7d759381d15e2f1df
Anne Whitman <aniel928@gmail.com>
2023-07-09 15:36:51 -0400

    add 404

--- src/App.js
+++ src/App.js
@@ -12,20 +12,23 @@ import CSS from './components/courses/CSS';
 import HTML from './components/courses/HTML';
 import JavaScript from './components/courses/JavaScript';
 
+import NotFound from './components/NotFound';
+
 function App() {
     return (
         <div className="container">
             <Header />
             <Routes>
                 <Route path='/' element={<Home />} />
-                <Route path='/about/*' element={<About />} />
-                <Route path='/teachers/*' element={<Teachers />} />
-                <Route path='/courses/*' element={<Courses />}>
+                <Route path='/about/' element={<About />} />
+                <Route path='/teachers/' element={<Teachers />} />
+                <Route path='/courses/' element={<Courses />}>
                     <Route index element={<Navigate replace to="html" />} />
                     <Route path='html' element={<HTML />} />
                     <Route path='css' element={<CSS />} />
                     <Route path='javascript' element={<JavaScript />} />
                 </Route>
+                <Route path='*' element={<NotFound />}></Route>
             </Routes>
         </div>
     );
