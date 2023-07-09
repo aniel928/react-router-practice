b7ad4b6c14bd78c6eb4a7122c4220aa842babcdc
Anne Whitman <aniel928@gmail.com>
2023-07-09 15:27:21 -0400

    add routes and links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 // App Components
 import About from './components/About';
@@ -8,15 +8,24 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Teachers from './components/Teachers';
 
+import CSS from './components/courses/CSS';
+import HTML from './components/courses/HTML';
+import JavaScript from './components/courses/JavaScript';
+
 function App() {
     return (
         <div className="container">
             <Header />
             <Routes>
                 <Route path='/' element={<Home />} />
-                <Route path='/about' element={<About />} />
-                <Route path='/teachers' element={<Teachers />} />
-                <Route path='/courses' element={<Courses />} />
+                <Route path='/about/*' element={<About />} />
+                <Route path='/teachers/*' element={<Teachers />} />
+                <Route path='/courses/*' element={<Courses />}>
+                    <Route index element={<Navigate replace to="html" />} />
+                    <Route path='html' element={<HTML />} />
+                    <Route path='css' element={<CSS />} />
+                    <Route path='javascript' element={<JavaScript />} />
+                </Route>
             </Routes>
         </div>
     );
